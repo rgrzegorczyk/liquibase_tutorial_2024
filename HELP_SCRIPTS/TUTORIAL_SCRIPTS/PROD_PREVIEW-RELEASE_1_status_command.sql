c0af9c4d4f253d170327edd3f439836f07835a19
Grzegorczyk Rafal <rgrzegorczyk@pretius.com>
2024-02-22 21:29:20 +0100

    release_1 to prod - deployment preview files

---
+++ HELP_SCRIPTS/TUTORIAL_SCRIPTS/PROD_PREVIEW-RELEASE_1_status_command.sql
@@ -0,0 +1,6 @@
+5 changesets have not been applied to HR@jdbc:oracle:thin:@prod_low
+     DATABASE/HR/TABLE/customers.sql::create_customers_table::rafal
+     DATABASE/HR/VIEW/emp_details_view_view.sql::emp_details_view_view::john
+     DATABASE/HR/REF_CONSTRAINT/customers_country_fk.sql::customers_country_fk::rafal
+     DATABASE/HR/PACKAGE_SPEC/COE_DOM_helper.sql::COE_DOM_helper_spec::john
+     DATABASE/HR/PACKAGE_BODY/COE_DOM_helper.sql::COE_DOM_helper_body::john
